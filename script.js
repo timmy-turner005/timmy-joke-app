c83bc07808c79c80e12588dcd01756ca74f7256d
timmy-turner005 <timitalented005@gmail.com>
2024-04-15 16:21:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const jokeEl = document.querySelector(".joke");
+const jokeBtn = document.querySelector(".joke-btn");
+
+function generateJoke() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://api.chucknorris.io/jokes/random");
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        jokeEl.innerHTML = JSON.parse(this.responseText).value;
+      } else {
+        jokeEl.innerHTML = "something is wrong.. lol joker";
+      }
+    }
+  };
+
+  xhr.send();
+}
+
+jokeBtn.addEventListener("click", generateJoke);
+document.addEventListener("DOMContentLoaded", generateJoke);
